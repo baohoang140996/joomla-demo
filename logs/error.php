6903672343f9a9d2f6c0984262bc714aef16e597
baohoang140996 <31727952+baohoang140996@users.noreply.github.com>
2017-09-07 16:30:30 +0700

    joomla commit

---
+++ logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-08-31 08:22:44 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2017-08-31	08:22:44	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2017-08-31	08:22:48	INFO	127.0.0.1	Joomla FAILURE: 	Empty password not allowed
+2017-08-31	08:23:01	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
---
+++ t3-assets/data.php
@@ -0,0 +1,17 @@
+<?php $filemap = array (
+  'salt' => 20498,
+  'css' => 
+  array (
+    '13a6c27c27035386e7865c3792bea8ef' => 1,
+    '4abd1ba962090e450405a48e3b3e8e68' => 1,
+    '7bd025a66b15f7b9b964848f58d295f4' => 1,
+    'ad6144937a3ba57030114b6e930a93a5' => 2,
+    'd661a9f27250d574bbb563e731f5d77e' => 1,
+    'db902261c501ee116c24afac58d8ac12' => 1,
+  ),
+  'js' => 
+  array (
+    '1ae08d78563c14eb26c96fd0a3a82bbf' => 1,
+    '4703ebf3aca72170d81fcb8c603d16e3' => 2,
+  ),
+); ?>
